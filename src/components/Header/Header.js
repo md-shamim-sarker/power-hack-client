84eaa68a188d3fc3fbf4e24ded5b8b7f1e053f35
shamimspro <shamimspro@gmail.com>
2023-01-29 02:53:53 +0600

    create header footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-2xl">This is header component.</h2>
+        <div className='flex justify-between px-10 py-2 bg-slate-300 items-center'>
+            <div className='flex items-center'>
+                <img src={logo} alt="Logo" className='w-12 h-12' title='POWER' />
+                <h2 className="text-3xl font-semibold">HACK</h2>
+            </div>
+            <div className='flex gap-5'>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/billings"}>Billings</NavLink>
+            </div>
+            <div>
+                <button className='btn btn-primary btn-sm'>Login</button>
+            </div>
         </div>
     );
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,23 +1,33 @@
 import {createBrowserRouter} from "react-router-dom";
+import Main from "../layouts/Main";
 import Billings from "../pages/Billings/Billings";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
 import Login from "../pages/LoginRegistration/Login";
 import Registration from "../pages/LoginRegistration/Registration";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Billings></Billings>,
-        errorElement: <ErrorPage></ErrorPage>
-    },
-    {
-        path: "/login",
-        element: <Login></Login>,
-        errorElement: <ErrorPage></ErrorPage>
-    },
-    {
-        path: "/register",
-        element: <Registration></Registration>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/billings",
+                element: <Billings></Billings>,
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Registration></Registration>,
+            }
+        ],
         errorElement: <ErrorPage></ErrorPage>
     }
 ]);
